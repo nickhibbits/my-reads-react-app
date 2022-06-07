16e436cef34e20a551a79cfdf805fdc25b2216c4
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-06-06 19:43:30 -0700

    organize directory, start Bookshelf component

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import "../css/App.css";
+import { useState } from "react";
+import Bookshelf from "./Bookshelf";
+
+function App() {
+  const [showSearchPage, setShowSearchpage] = useState(false);
+
+  return (
+    <div className="app">
+      {showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a
+              className="close-search"
+              onClick={() => setShowSearchpage(!showSearchPage)}
+            >
+              Close
+            </a>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title, author, or ISBN"
+              />
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      ) : (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <Bookshelf title={"Currently Reading"} />
+              <Bookshelf title={"Want to Read"} />
+              <Bookshelf title={"Read"} />
+            </div>
+          </div>
+          <div className="open-search">
+            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./css/App.css";
+import "./css/index.css";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
